ea656cde30a0c7ed3b40c79cbe34de7b5b57879a
IsmaAlfaro <ismaelalfaro119@gmail.com>
2026-08-21 00:13:40 -0600

    cambio de nombre de variable
    para una mejor aplicacion y entendimiento pasamos de llamar documento a identificacion en la clase cliente

--- src/alquilercarros/modelo/Cliente.java
+++ src/alquilercarros/modelo/Cliente.java
@@ -1,27 +1,27 @@
 package alquilercarros.modelo;
 
 public class Cliente {
-    private final String documento;
+    private final String identificacion;
     private final String nombreCompleto;
     private String telefono;
     private String correo;
 
-    public Cliente(String documento, String nombreCompleto, String telefono, String correo) {
-        if (documento == null || documento.trim().isEmpty()) {
-            throw new IllegalArgumentException("El documento es obligatorio.");
+    public Cliente(String identificacion, String nombreCompleto, String telefono, String correo) {
+        if (identificacion == null || identificacion.trim().isEmpty()) {
+            throw new IllegalArgumentException("La identificacion es obligatoria.");
         }
         if (nombreCompleto == null || nombreCompleto.trim().isEmpty()) {
             throw new IllegalArgumentException("El nombre es obligatorio.");
         }
 
-        this.documento = documento.trim();
+        this.identificacion = identificacion.trim();
         this.nombreCompleto = nombreCompleto.trim();
         setTelefono(telefono);
         setCorreo(correo);
     }
 
-    public String getDocumento() {
-        return documento;
+    public String getIdentificacion() {
+        return identificacion;
     }
 
     public String getNombreCompleto() {
@@ -52,7 +52,7 @@ public class Cliente {
 
     @Override
     public String toString() {
-        return documento + " - " + nombreCompleto + " | Telefono: " + telefono
+        return identificacion + " - " + nombreCompleto + " | Telefono: " + telefono
                 + " | Correo: " + correo;
     }
 }
